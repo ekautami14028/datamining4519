50faecbd4f4d899a182e91d294f540a4112ccde0
ekautami14028 <113227467+ekautami14028@users.noreply.github.com>
2025-01-16 20:13:43 +0700

    Delete UAS_DM/install_sklearn.py

--- UAS_DM/install_sklearn.py
+++
@@ -1,9 +0,0 @@
-import os
-
-try:
-    import sklearn
-    print("Scikit-learn sudah terinstal.")
-except ImportError:
-    print("Menginstal scikit-learn...")
-    os.system("pip install scikit-learn")
-    print("Scikit-learn berhasil diinstal.")
